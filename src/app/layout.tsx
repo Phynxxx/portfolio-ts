e840a94143199182264624590441b521a480feea
Phynxxx <mail2syamantak@gmail.com>
2024-04-26 11:16:47 +0530

    Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Syamnatak's Portfolio",
   description: "Syamnatak's Portfolio",
+  metadataBase: new URL('https://syamantak.vercel.app/'),
 };
 
 export default function RootLayout({
